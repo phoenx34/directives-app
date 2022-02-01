d0ff8f625dcc1d3feb92bae8b5fabd07cae26a2b
phoenx34 <misaac34@gmail.com>
2022-02-01 15:24:50 -0500

    [7-2] Custom Attribute Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 import { AppComponent } from './app.component';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirective,
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule
